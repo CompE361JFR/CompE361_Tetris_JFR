ca63944e4b456468ecb282914bf6707012f78876
CompE361JFR <45084213+CompE361JFR@users.noreply.github.com>
2018-11-15 17:20:43 -0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+
+
+
+
+// Note to self: Insert Image Tutorial: 
+// https://docs.microsoft.com/en-us/previous-versions/windows/apps/hh965322(v=win.10)
+
+// Tetris logo soure:
+// https://playzones.files.wordpress.com/2009/11/tetris-logo.jpg
+
+namespace Tetris_v0_3r
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
